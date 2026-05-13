e594d1e919b9bdb17adc6759304591fd54f87f47
rudrapulami <pulamirudra16@gmail.com>
2026-05-13 15:39:13 +0545

    generate 3d design

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,20 +1,116 @@
-import { useLocation } from "react-router";
+import { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { generate3DView } from "../../lib/ai.action";
+import { Box, Download, Share2, RefreshCcw, X } from "lucide-react";
+import { Button } from "../../components/ui";
 
 const VisualizerId = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-  const { initialImage, name } = location.state || {};
+  const { initialImage, initialRender, name } = location.state || {};
+
+  const hasInitialGenerated = useRef(false);
+  const [currentImage, setCurrentImage] = useState<string | null>(initialRender || null);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleBack = () => navigate("/");
+
+  const runGeneration = async () => {
+    if (!initialImage) return;
+
+    try {
+      setIsProcessing(true);
+      const result = await generate3DView({ sourceImage: initialImage });
+
+      if (result.renderedImage) {
+        setCurrentImage(result.renderedImage);
+
+        // update the project with rendered image.
+      }
+    } catch (error) {
+      console.error("Generation failed", error);
+    } finally {
+      setIsProcessing(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!initialImage || hasInitialGenerated.current) return;
+
+    if (initialRender) {
+      setCurrentImage(initialRender);
+      hasInitialGenerated.current = true;
+      return;
+    }
+
+    hasInitialGenerated.current = true;
+    runGeneration();
+  }, [initialImage, initialRender]);
+
   return (
-    <section>
-      <h1> {name || "Untitled Project"} </h1>
-      <div className="visualizer">
-        {initialImage && (
-          <div className="image-container">
-            <h2>Source Image</h2>
-            <img src={initialImage} alt="source" />
+    <div className="visualizer">
+      <nav className="topbar">
+        <div className="brand">
+          <Box className="logo" />
+
+          <span className="name">Roomify</span>
+        </div>
+        <Button variant="ghost" size="sm" onClick={handleBack}>
+          <X className="icon" />
+          Exit Editor
+        </Button>
+      </nav>
+
+      <section className="content">
+        <div className="panel">
+          <div className="panel-header">
+            <div className="panel-meta">
+              <p>Project</p>
+              <h2>{"Untitled Project"}</h2>
+              <p className="note">Created by You</p>
+            </div>
+
+            <div className="panel-actions">
+              <Button
+                size="sm"
+                onClick={() => {}}
+                className="expert"
+                disabled={!currentImage || isProcessing}
+              >
+                <Download className="mr-2 h-4 w-4" />
+                Export
+              </Button>
+              <Button size="sm" onClick={() => {}} className="share">
+                <Share2 className="mr-2 h-4 w-4" />
+                Share
+              </Button>
+            </div>
           </div>
-        )}
-      </div>
-    </section>
+
+          <div className={`rendeer-area ${isProcessing ? "is-processing" : ""}`}>
+            {currentImage ? (
+              <img src={currentImage} alt="AI Render" className="render-img" />
+            ) : (
+              <div className="render-placeholder">
+                {initialImage && (
+                  <img src={initialImage} alt="Original" className="render-fallback" />
+                )}
+              </div>
+            )}
+
+            {isProcessing && (
+              <div className="render-overlay">
+                <div className="rendering-card">
+                  <RefreshCcw className="spinner" />
+                  <span className="title">Rendering...</span>
+                  <span className="subtitle">Generating your 3D visialization</span>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
